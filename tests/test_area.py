b76bd80a4f822edd1cc70490493392b99c8e34b4
ycdanielx <78879500+ycdanielx@users.noreply.github.com>
2024-02-23 12:59:03 -0500

    pythontesting

--- tests/test_area.py
+++ tests/test_area.py
@@ -4,7 +4,7 @@ from src.area import calculate_area_square
 def test_calculate_area_square():  
     assert calculate_area_square(2) == 4  
     assert calculate_area_square(2.5) == 6.25
-    assert calculate_area_square(3) == 38    
+    assert calculate_area_square(3) == 9    
   
 def test_calculate_area_square_negative():  
     with pytest.raises(TypeError):  
